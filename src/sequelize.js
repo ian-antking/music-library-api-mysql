1ca4d8b0aa151c29b7538cfb63754ce5aa907fc1
Ian King <36984875+ian-antking@users.noreply.github.com>
2020-05-11 21:45:49 +0100

    comment out album and song require statements

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const ArtistModel = require('./models/artist');
-const AlbumModel = require('./models/album');
-const SongModel = require('./models/song');
+// const AlbumModel = require('./models/album');
+// const SongModel = require('./models/song');
 
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, CLEARDB_DATABASE_URL } = process.env;
 
